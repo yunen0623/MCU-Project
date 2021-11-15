ff76daaa76f83ca92ac1e4f7b3a4990d8910094a
yunen0623 <72005338+yunen0623@users.noreply.github.com>
2021-11-15 15:27:53 +0800

    Add files via upload

---
+++ 8051-Intergation(Int)/8051-Intergation(Int).c
@@ -0,0 +1,54 @@
+#include <reg51.h>
+#define LED P1
+void delay1ms(int);
+void runled(int);
+//void debouncer(void);
+main()
+{
+	IE = 0x81;
+	IT0 = 0;
+	P1 = 0xD5;
+	while(1)
+	{
+		delay1ms(250); //0.25s 
+		P1 = ~P1;
+	}
+}
+
+void INT0Int(void) interrupt 0
+{
+	unsigned saveLED=LED;
+	runled(6);
+	LED = saveLED;
+}
+
+void runled(int x)
+{
+	int i ;
+	unsigned char j;
+	P1=0xFE;
+		for(j=0;j<x;j++)
+		{
+			for(i=0;i<30000;i++);
+			P1=(P1<<1)|0x01;
+		}
+		for(j=0;j<x;j++)
+		{
+			for(i=0;i<30000;i++);
+			P1=(P1>>1)|0x80;
+		}
+		delay1ms(250);
+}
+
+void delay1ms(int x)
+{
+	int i,j;
+	for(i=0;i<x;i++)
+	for(j=0;j<250;j++);
+}
+
+/*void debouncer(void)
+{
+	int i;
+	for(i=0;i<2400;i++);
+}*/
